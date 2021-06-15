6f52f2baaa88cfc8ec1573df81f0f6b98090eca5
Jackson Anderson <jda4923@rit.edu>
2021-06-15 16:30:42 -0400

    focused IDT initial work

--- cells.py
+++ cells.py
@@ -5,17 +5,17 @@ import numpy as np
 def gsg_pad(lib, layers=[0,1], p=150, x=80, y=70):
     c = lib.new_cell(f'GSG_pad_P{p}_x{x}_y{y}')
     #  signal pad
-    c.add(gd.Rectangle((-x/2, -y/2), (x/2, y/2), layer=layers[1]))
+    c.add(gd.Rectangle((-x/2, -y/2), (x/2, y/2), layer=layers['M2']))
     # rt = gd.Polygon([(x/2, -y/2),(x/2+40, -y/2+10),(x/2+40, y/2-10),(x/2, y/2)])
 
     #  ground pads
-    c.add(gd.Rectangle((-x/2-20, -y/2-p), (x/2, y/2-p), layer=layers[1]))
-    c.add(gd.Rectangle((-x/2-20, -y/2+p), (x/2, y/2+p), layer=layers[1]))
+    c.add(gd.Rectangle((-x/2-20, -y/2-p), (x/2, y/2-p), layer=layers['M2']))
+    c.add(gd.Rectangle((-x/2-20, -y/2+p), (x/2, y/2+p), layer=layers['M2']))
 
-    c.add(gd.Rectangle((-x/2-40, -y/2-p), (-x/2-20, y/2+p), layer=layers[1]))
+    c.add(gd.Rectangle((-x/2-40, -y/2-p), (-x/2-20, y/2+p), layer=layers['M2']))
     return c
 
-def bar_resonator(lib, pads, layers=[0, 1], d=150, x=30, y=200, yoff=40, trench=False):
+def bar_resonator(lib, pads, layers, d=150, x=30, y=200, yoff=40, trench=False):
     """
 
     :param lib:
@@ -29,8 +29,8 @@ def bar_resonator(lib, pads, layers=[0, 1], d=150, x=30, y=200, yoff=40, trench=
     :return: c: bar resonator cell
     """
     c = lib.new_cell(f'bar_res_d{d}_x{x}_y{y}')
-    c.add(gd.Rectangle((-d/2-x, -y/2-yoff), (-d/2, y/2), layers[1]))
-    c.add(gd.Rectangle((d / 2, -y / 2), (d / 2 + x, y / 2+yoff), layers[1]))
+    c.add(gd.Rectangle((-d/2-x, -y/2-yoff), (-d/2, y/2), layers['M1']))
+    c.add(gd.Rectangle((d / 2, -y / 2), (d / 2 + x, y / 2+yoff), layers['M1']))
     pad_extent = gd.CellReference(pads).get_bounding_box()
     c.add(gd.CellReference(pads,
                            rotation=-90,
@@ -47,14 +47,43 @@ def bar_resonator(lib, pads, layers=[0, 1], d=150, x=30, y=200, yoff=40, trench=
                       (-d / 2 - (x - 70) / 2, -y / 2 - 30-yoff),
                       (-d / 2 - x + (x - 70) / 2, -y / 2 - 30-yoff),
                       (-d / 2 - x, -y / 2-yoff)
-                      ], layers[1]))
+                      ], layers['M1']))
 
 
     if trench and d>10:
-        c.add(gd.Rectangle((-d/2+5, -y/2-yoff/2), (d/2-5, y/2+yoff/2), layers[0]))
+        c.add(gd.Rectangle((-d/2+5, -y/2-yoff/2), (d/2-5, y/2+yoff/2), layers['Trench']))
     extent = c.get_bounding_box()
-    c.add(gd.Text(f'bar_res_d{d}_x{x}_y{y}', 20, (extent[0][0], extent[1][1]+5), layers[1]))
+    c.add(gd.Text(f'bar_res_d{d}_x{x}_y{y}', 20, (extent[0][0], extent[1][1]+5), layers['M1']))
     return c
+
+
+def bar_resonator_matrix(lib, pads, layers, ds, ys, trench=False):
+    m = lib.new_cell(f'Resonator Matrix')
+    max_x = 0
+    max_y = 0
+    cells = []
+    x0 = []
+    y0 = []
+    for i, d in enumerate(ds):
+        for j, y in enumerate(ys):
+            c = bar_resonator(lib, pads, layers, d=d, y=y, trench=trench)
+            x0.append(i)
+            y0.append(j)
+            cells.append(c)
+            extent = c.get_bounding_box()
+            if extent[1][0] - extent[0][0] > max_x:
+                max_x = extent[1][0] - extent[0][0]
+            else:
+                pass
+            if extent[1][1] - extent[0][1] > max_y:
+                max_y = extent[1][1] - extent[0][1]
+            else:
+                pass
+    x0 = (np.asarray(x0) * max_x * 1.1)
+    y0 = (np.asarray(y0) * max_y * 1.1)
+    for i, r in enumerate(cells):
+        m.add(gd.CellReference(r, origin=(x0[i], y0[i])))
+
 
 def idt_device(lib, pads, layers, lmda, g_idt, idt_type='standard', process_bias=0, n_idt=20, w_b=20, s_b=5, l_idt=None,
         reflector=False, g_r=None, w_br=None, n_idtr=None, w_r=None, s_r=None):
@@ -66,16 +95,16 @@ def idt_device(lib, pads, layers, lmda, g_idt, idt_type='standard', process_bias
         cell_name = 'dIDT'
         w_idt = lmda / 4 + process_bias
         s_idt = lmda / 4 - process_bias
-        idts = idt_array(lib, layers, w_idt, s_idt, l_idt, n_idt, w_b, s_b)
+        idts = idt_cell(lib, layers, w_idt, s_idt, l_idt, n_idt, w_b, s_b)
     elif idt_type == 'EWC':
         cell_name = 'dEWC'
-        idts = ewc_array(lib, layers,lmda, process_bias, l_idt, n_idt, w_b, s_b)
+        idts = ewc_cell(lib, layers, lmda, process_bias, l_idt, n_idt, w_b, s_b)
     elif idt_type == 'split':
         cell_name = 'dSplitFing'
-        idts = split_finger_array(lib, layers, lmda, process_bias, l_idt, n_idt, w_b, s_b)
+        idts = split_finger_cell(lib, layers, lmda, process_bias, l_idt, n_idt, w_b, s_b)
     elif idt_type == 'dart':
         cell_name = 'dDART'
-        idts = dart_array(lib, layers, lmda, process_bias, l_idt, n_idt, w_b, s_b)
+        idts = dart_cell(lib, layers, lmda, process_bias, l_idt, n_idt, w_b, s_b)
     cell_name = cell_name+f'_lambda{lmda:.1f}_g{g_idt:.1f}_nIDT{n_idt:.1f}_IDTl{l_idt:.1f}_wb{w_b:.1f}_sb{s_b:.1f}_bias{process_bias:.1f}'
     if reflector is True:
         if g_r is None: g_r = 5*lmda # TODO: Change default value based on simulated performance
@@ -87,20 +116,42 @@ def idt_device(lib, pads, layers, lmda, g_idt, idt_type='standard', process_bias
 
     # Cell Creation
     c = lib.new_cell(cell_name)
-    idt_extent = gd.CellReference(idts).get_bounding_box()
+    idt_extent = idts.get_bounding_box()
     idt_offset = g_idt/2 + idt_extent[1][0]
-    c.add(gd.CellReference(idts, origin=(-idt_offset, 0)))
-    c.add(gd.CellReference(idts, origin=(idt_offset, 0), rotation=180, x_reflection=True))
+    c.add(gd.CellReference(idts, origin=(-idt_offset, 0), rotation=180)) # rotation puts idt finger at gap edge
+    c.add(gd.CellReference(idts, origin=(idt_offset, 0), rotation=0))
     if reflector is True:
         r = idt_reflector(lib, layers, w_r, s_r, l_idt, n_idtr, w_br)
         r_extent = gd.CellReference(r).get_bounding_box()
         r_offset = g_idt/2 + (idt_extent[1][0]-idt_extent[0][0]) + g_r + r_extent[1][0]
         c.add(gd.CellReference(r, origin=(r_offset, 0)))
-        c.add(gd.CellReference(r, origin=(-r_offset, 0)))
+        c.add(gd.CellReference(r, origin=(-r_offset, 0), rotation=180))
+
+    # generate pads
+    pad_extent = pads.get_bounding_box()
+    pad_tapery = 50  # offset distance for tapering from pad edge to IDT busbar
+    v1_y = 20
+    pad_yoffset = idt_extent[1][1]+pad_extent[1][0]+pad_tapery+ v1_y
+    for (i, j) in [(-1, 1), (1, -1)]:
+        c.add(gd.CellReference(pads,
+                               rotation=i*90,
+                               origin=(i*idt_offset, j*pad_yoffset)))
+        c.add(gd.Polygon([(i*(idt_offset -n_idt * lmda/2), j*idt_extent[1][1]),
+                          (i*(idt_offset-35), j*(idt_extent[1][1]+pad_tapery)),
+                          (i*(idt_offset+35), j*(idt_extent[1][1]+pad_tapery)),
+                          (i*(idt_offset + n_idt*lmda/2), j*idt_extent[1][1]),
+                          ], layers['M1']))
+        c.add(gd.Rectangle((i*(idt_offset-35), j*(idt_extent[1][1]+pad_tapery)),
+                           (i*(idt_offset+35), j*(idt_extent[1][1]+pad_tapery + v1_y)),
+                           layers['M2']))
+        c.add(gd.Rectangle((i*(idt_offset-35), j*(idt_extent[1][1]+pad_tapery)),
+                           (i*(idt_offset+35), j*(idt_extent[1][1]+pad_tapery + v1_y)),
+                           layers['M1']))
+
     return c
 
 
-def idt_array(lib, layers, w_idt, s_idt, l_idt, n_idt, w_b, s_b, label='IDT'):
+def idt_cell(lib, layers, w_idt, s_idt, l_idt, n_idt, w_b, s_b, label='IDT'):
     cellname = label+f'_wIDT{w_idt}_sIDT{s_idt}_nIDT{n_idt:.1f}_lIDT{l_idt:.1f}_wb{w_b:.1f}_sb{s_b:.1f}'
     try:
         c = lib.new_cell(cellname)
@@ -110,15 +161,48 @@ def idt_array(lib, layers, w_idt, s_idt, l_idt, n_idt, w_b, s_b, label='IDT'):
         xoff = i*2*(w_idt+s_idt)-n_idt*(w_idt+s_idt)
         c.add(gd.Rectangle((xoff, -l_idt/2), (xoff+w_idt, l_idt/2-s_b), layers['M1']))
         c.add(gd.Rectangle((xoff+w_idt+s_idt, -l_idt / 2+s_b), (xoff+2*w_idt+s_idt, l_idt/2), layers['M1']))
+    # idt_extents = c.get_bounding_box()
+    # c.add(gd.Rectangle((idt_extents[0][0], idt_extents[1][1]),
+    #                    (idt_extents[1][0], idt_extents[1][1]+w_b),
+    #                    layers['M1']))
+    # c.add(gd.Rectangle((idt_extents[0][0], idt_extents[0][1]),
+    #                    (idt_extents[1][0], idt_extents[0][1] - w_b),
+    #                    layers['M1']))
     c.add(gd.Rectangle((-n_idt*(w_idt+s_idt), l_idt/2), (n_idt*(w_idt+s_idt), l_idt/2+w_b), layers['M1']))
     c.add(gd.Rectangle((-n_idt * (w_idt + s_idt), -l_idt / 2), (n_idt * (w_idt + s_idt), -l_idt / 2 - w_b), layers['M1']))
     return c
 
+def focused_idt_cell(lib, layers, w_idt, s_idt, delay, theta, g_idt, n_idt, w_b, s_b, label='focIDT'):
+    cellname = label+f'_wIDT{w_idt}_sIDT{s_idt}_nIDT{n_idt:.1f}delay{delay:.1f}_theta{theta:0.1f}(g_idt{g_idt:0.1f}_wb{w_b:.1f}_sb{s_b:.1f}'
+    try:
+        c = lib.new_cell(cellname)
+    except ValueError:
+        return lib.cells[cellname]
+    c.add(gd.Round((0,0),delay/2+w_idt,delay/2,-theta*np.pi/360,theta*np.pi/360, layer=layers['M1']))
+
+    for i in range(n_idt):
+        xoff = delay/2 + i*2*(w_idt+s_idt)-n_idt*(w_idt+s_idt)
+        c.add(gd.Round((0, 0), delay / 2 + xoff+w_idt, delay/2 + xoff, -theta * np.pi / 360, theta * np.pi / 360,
+                       layer=layers['M1']))
+        # c.add(gd.Rectangle((xoff, -l_idt/2), (xoff+w_idt, l_idt/2-s_b), layers['M1']))
+        # c.add(gd.Rectangle((xoff+w_idt+s_idt, -l_idt / 2+s_b), (xoff+2*w_idt+s_idt, l_idt/2), layers['M1']))
+    c.add(gd.Rectangle((delay/2, 0), (delay/2+n_idt*2*(w_idt+s_idt), w_b), layers['M1']).rotate(theta * np.pi / 360))
+    c.add(gd.Rectangle((delay / 2, 0), (delay / 2 + n_idt * 2 * (w_idt + s_idt),-w_b), layers['M1']).rotate(-theta * np.pi / 360))
+    # idt_extents = c.get_bounding_box()
+    # c.add(gd.Rectangle((idt_extents[0][0], idt_extents[1][1]),
+    #                    (idt_extents[1][0], idt_extents[1][1]+w_b),
+    #                    layers['M1']))
+    # c.add(gd.Rectangle((idt_extents[0][0], idt_extents[0][1]),
+    #                    (idt_extents[1][0], idt_extents[0][1] - w_b),
+    #                    layers['M1']))
+
+    return c
+
 def idt_reflector(lib, layers, w_idt, s_idt, l_idt, n_idt, w_b):
-    r = idt_array(lib, layers, w_idt, s_idt, l_idt, n_idt, w_b, s_b=0, label='IDT_reflector')
+    r = idt_cell(lib, layers, w_idt, s_idt, l_idt, n_idt, w_b, s_b=0, label='IDT_reflector')
     return r
 
-def ewc_array(lib, layers, lmda, process_bias, l_idt, n_idt, w_b, s_b):
+def ewc_cell(lib, layers, lmda, process_bias, l_idt, n_idt, w_b, s_b):
     #TODO: check phase for delay between two IDTs
     cellname = f'EWC_lamda{lmda:.1f}_bias{process_bias:.1f}_nIDT{n_idt:.1f}_lIDT{l_idt:.1f}_wb{w_b:.1f}_sb{s_b:.1f}'
     try:
@@ -135,7 +219,7 @@ def ewc_array(lib, layers, lmda, process_bias, l_idt, n_idt, w_b, s_b):
         gd.Rectangle((-n_idt * lmda/2, -l_idt / 2), (n_idt * lmda/2, -l_idt / 2 - w_b), layers['M1']))
     return c
 
-def dart_array(lib, layers, lmda, process_bias, l_idt, n_idt, w_b, s_b):
+def dart_cell(lib, layers, lmda, process_bias, l_idt, n_idt, w_b, s_b):
     #TODO: check phase for delay between two IDTs
     cellname = f'DART_lamda{lmda:.1f}_bias{process_bias:.1f}_nIDT{n_idt:.1f}_lIDT{l_idt:.1f}_wb{w_b:.1f}_sb{s_b:.1f}'
     try:
@@ -152,7 +236,8 @@ def dart_array(lib, layers, lmda, process_bias, l_idt, n_idt, w_b, s_b):
         gd.Rectangle((-n_idt * lmda/2, -l_idt / 2), (n_idt * lmda/2, -l_idt / 2 - w_b), layers['M1']))
     return c
 
-def split_finger_array(lib, layers, lmda, process_bias, l_idt, n_idt, w_b, s_b):
+
+def split_finger_cell(lib, layers, lmda, process_bias, l_idt, n_idt, w_b, s_b):
     #TODO: check phase for delay between two IDTs
     cellname = f'splitfinger_lamda{lmda:.1f}_bias{process_bias:.1f}_nIDT{n_idt:.1f}_lIDT{l_idt:.1f}_wb{w_b:.1f}_sb{s_b:.1f}'
     try:
@@ -171,32 +256,6 @@ def split_finger_array(lib, layers, lmda, process_bias, l_idt, n_idt, w_b, s_b):
     return c
 
 
-def bar_resonator_matrix(lib, pads, ds, ys, trench=False):
-    m = lib.new_cell(f'Resonator Matrix')
-    max_x = 0
-    max_y = 0
-    cells = []
-    x0 = []
-    y0 = []
-    for i, d in enumerate(ds):
-        for j, y in enumerate(ys):
-            c = bar_resonator(lib, pads, d=d, y=y, trench=trench)
-            x0.append(i)
-            y0.append(j)
-            cells.append(c)
-            extent = c.get_bounding_box()
-            if extent[1][0] - extent[0][0] > max_x:
-                max_x = extent[1][0] - extent[0][0]
-            else:
-                pass
-            if extent[1][1] - extent[0][1] > max_y:
-                max_y = extent[1][1] - extent[0][1]
-            else:
-                pass
-    x0 = (np.asarray(x0) * max_x * 1.1)
-    y0 = (np.asarray(y0) * max_y * 1.1)
-    for i, r in enumerate(cells):
-        m.add(gd.CellReference(r, origin=(x0[i], y0[i])))
 
 # WIP
 def alignment_marks(lib, layers):
@@ -214,13 +273,19 @@ lib = gd.GdsLibrary('Resonator Library')
 gd.current_library = lib
 ds = np.linspace(10, 100, 7)
 ys = np.linspace(100, 200, 6)
-pads = gsg_pad(lib)
 
-#bar_resonator_matrix(lib, pads, ds, ys, trench=True)
-test_idt = idt_device(lib, pads,{'M1':1}, 1, g_idt=10, idt_type='standard', l_idt=50, reflector=True, )
-test_idt2 = idt_device(lib, pads,{'M1':1}, 1, g_idt=10, idt_type='EWC', l_idt=50, reflector=True, )
-test_idt3 = idt_device(lib, pads,{'M1':1}, 1, g_idt=10, idt_type='split', l_idt=50, reflector=True, )
-test_idt4 = idt_device(lib, pads,{'M1':1}, 1, g_idt=10, idt_type='dart', l_idt=50, reflector=True, )
+layers = {
+    'Trench': 0,
+    'M1': 1,
+    'M2': 2,
+    }
+
+
+pads = gsg_pad(lib, layers)
+#bar_resonator_matrix(lib, pads, layers, ds, ys, trench=True)
+for i in ['standard', 'EWC', 'split', 'dart']:
+    test_idt = idt_device(lib, pads, layers, 1, g_idt=3, idt_type=i, l_idt=100, reflector=True, g_r=10, n_idtr=50)
+focused_idt_cell(lib,layers,.25,.25,20,60,.25,20,10,3)
 gd.LayoutViewer(lib)
 
 #%%
